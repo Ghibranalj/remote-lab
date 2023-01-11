d310ebf03fa4e3b8d48fb127953022c9f728a708
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-01-11 14:21:39 +0100

    initital commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+setuprouteros() {
+    mkdir routeros
+    cd routeros || exit
+    wget 'https://download.mikrotik.com/routeros/7.6/chr-7.6.vdi.zip'
+    unzip chr-7.6.vdi.zip
+    mv chr-7.6.vdi chr.vdi
+    cd ..
+}
+[ -f "./routeros/chr.vdi" ] || setuprouteros
+mkdir -p ./mikrorad
+
