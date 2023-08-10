2ffa96f1e1ef2570cc0111f74431f6425bf98a20
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-08-10 22:08:39 +0200

    fix: python -> python3 & change ln to cp

--- setup.sh
+++ setup.sh
@@ -13,12 +13,12 @@ setuprouteros() {
 
 sudo add-apt-repository ppa:gns3/ppa
 sudo apt update
-sudo apt install gns3-gui gns3-server python nginx novnc websockify
+sudo apt install gns3-gui gns3-server python3 nginx novnc websockify
 # check if you have docker
 [ -x "docker version" ] && snap install docker
 
 sudo mv /etc/nginx/nginx.conf{,.bak}
-sudo ln $PWD/nginx.conf /etc/nginx/nginx.conf
+sudo cp $PWD/nginx.conf /etc/nginx/nginx.conf
 
 cat <<EOF > server.conf
 [Server]
